cc9a4c7e6b2f9a1633f033b904435f8783d8ecf4
Anthea Yur <acyur6@gmail.com>
2024-01-05 12:05:20 -0600

    Remove previous lines and reduce

--- app/controllers/api/v0/spaces_controller.rb
+++ app/controllers/api/v0/spaces_controller.rb
@@ -13,29 +13,6 @@ class Api::V0::SpacesController < ApplicationController
   def create_third_space
     id = params[:id]
 
-    # conn = Faraday.new(url: "https://api.yelp.com/v3/businesses/") do |faraday|
-    #   faraday.headers["Authorization"] = Rails.application.credentials.yelp[:key]
-    # end
-
-    # response = conn.get("#{id}")
-    # data = JSON.parse(response.body, symbolize_names: true)
-
-    # third_space = ThirdSpace.new(
-    #                             yelp_id: id,
-    #                             name: data[:name],
-    #                             address: data[:location][:display_address].join(", "), 
-    #                             rating: data[:rating],
-    #                             phone: data[:display_phone],
-    #                             photos: data[:photos],
-    #                             lat: data[:coordinates][:latitude],
-    #                             lon: data[:coordinates][:longitude],
-    #                             price: data[:price],
-    #                             hours: data[:hours],
-    #                             open_now: data[:hours].first[:is_open_now],  #Need to Clean this Up
-    #                             category: data[:categories].first[:title],
-    #                             tags: []
-    #                             )
-
     facade = SpaceCreateFacade.new
     third_space = facade.get_space_details(id)
 
