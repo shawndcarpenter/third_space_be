32a91b32b2c8c8c73ffc904b3350906cee6bed01
Anthea Yur <acyur6@gmail.com>
2024-01-05 00:28:35 -0600

    Create create_third_space method

--- app/controllers/api/v0/spaces_controller.rb
+++ app/controllers/api/v0/spaces_controller.rb
@@ -18,6 +18,21 @@ class Api::V0::SpacesController < ApplicationController
     render json: SearchResultSerializer.new(search_results).serializable_hash
   end
 
+  def create_third_space(id)
+    conn = Faraday.new(url: "https://api.yelp.com/v3/businesses/") do |faraday|
+      faraday.headers["Authorization"] = Rails.application.credentials.yelp[:key]
+    end
+
+    response = conn.get("#{id}")
+    data = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+
+    third_space = data.map do |details|
+      ThirdSpace.new(details)
+    end
+
+  end
+
   private
 
   def search_params
