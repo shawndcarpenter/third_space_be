731fb8c74c8f3e3b4b6ab3dc14e2aae6bbfd3d12
Anthea Yur <acyur6@gmail.com>
2024-01-05 12:16:27 -0600

    Change Model to Poro for Third Space entry

--- app/facades/space_create_facade.rb
+++ app/facades/space_create_facade.rb
@@ -6,21 +6,21 @@ class SpaceCreateFacade
 
   def parse_json(data)
     data = JSON.parse(data.body, symbolize_names: true)
-    third_space = ThirdSpace.new(
+    third_space = ThirdSpace.new(data
       
-                                yelp_id: data[:id],
-                                name: data[:name],
-                                address: data[:location][:display_address].join(", "), 
-                                rating: data[:rating],
-                                phone: data[:display_phone],
-                                photos: data[:photos],
-                                lat: data[:coordinates][:latitude],
-                                lon: data[:coordinates][:longitude],
-                                price: data[:price],
-                                hours: data[:hours],
-                                open_now: data[:hours].first[:is_open_now],  #Need to Clean this Up
-                                category: data[:categories].first[:title],
-                                tags: []
+                                # yelp_id: data[:id],
+                                # name: data[:name],
+                                # address: data[:location][:display_address].join(", "), 
+                                # rating: data[:rating],
+                                # phone: data[:display_phone],
+                                # photos: data[:photos],
+                                # lat: data[:coordinates][:latitude],
+                                # lon: data[:coordinates][:longitude],
+                                # price: data[:price],
+                                # hours: data[:hours],
+                                # open_now: data[:hours].first[:is_open_now],  #Need to Clean this Up
+                                # category: data[:categories].first[:title],
+                                # tags: []
                                 )
   end
 end
--- app/models/third_space.rb
+++
@@ -1,5 +0,0 @@
-class ThirdSpace < ApplicationRecord
-  validates :yelp_id, :name, :address, :rating, :phone, :photos, :lat, :lon, :price, :hours, :category, presence: true
-  validates :open_now, inclusion: { in: [true, false]}
-  validates :tags, presence: { allow_blank: true }
-end
