018440b00061c4eae1ebd492ec79449d5d132899
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-06 10:15:06 -0700

    Refactor controller to use new facade names

--- app/controllers/api/v0/spaces_controller.rb
+++ app/controllers/api/v0/spaces_controller.rb
@@ -4,26 +4,22 @@ class Api::V0::SpacesController < ApplicationController
     name = search_params[:name]
     city = search_params[:city]
 
-    facade = SpaceSearchFacade.new
-    search_results = facade.get_search_results(name, city)
+    locations = LocationSearchFacade.new(name, city).locations
 
-    render json: SearchResultSerializer.new(search_results).serializable_hash
+    render json: SearchResultSerializer.new(locations)
   end
 
   def create_third_space
     id = params[:id]
 
-    facade = SpaceCreateFacade.new
+    facade = ThirdSpaceFacade.new
     third_space = facade.get_space_details(id)
 
     render json: ThirdSpaceSerializer.new(third_space).serializable_hash
   end
 
   private
-
   def search_params
     params.permit(:name, :city)
   end
-
-
 end
