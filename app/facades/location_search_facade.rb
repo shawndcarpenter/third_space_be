b932ddac718a0191cd4bd18bc5c47eab11d2cb54
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-06 10:32:18 -0700

    Remove pry

--- app/facades/location_search_facade.rb
+++ app/facades/location_search_facade.rb
@@ -7,7 +7,6 @@ class LocationSearchFacade
   def locations
     service = LocationSearchService.new
     json = service.find_locations(@name, @city)
-    # binding.pry
     @locations = json.map do |location|
       SearchResult.new(location)
     end
