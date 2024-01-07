2f9e4de2feac8699792d927777f5331ee8a03b53
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-06 18:36:45 -0700

    Add new tags to poro and serializer

--- app/poros/third_space_poro.rb
+++ app/poros/third_space_poro.rb
@@ -12,20 +12,7 @@ class ThirdSpacePoro
               :hours, 
               :category, 
               :tags, 
-              :open_now, 
-              :gender_neutral_restrooms, 
-              :volume, 
-              :accessible_entrance, 
-              :customer_restrooms, 
-              :parking, 
-              :purchase_necessary, 
-              :sober, 
-              :child_friendly, 
-              :light_level, 
-              :public_transportation_nearby, 
-              :bipoc_friendly, 
-              :queer_friendly, 
-              :staff_responsiveness
+              :open_now
 
   def initialize(data)
     @yelp_id = data[:id]
