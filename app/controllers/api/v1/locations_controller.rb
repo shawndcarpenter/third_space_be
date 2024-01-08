a5043a4004eefe30805102e4b9a33b5e5a146f7c
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-08 10:39:44 -0700

    Add show method with error handling for detailed locations

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -8,8 +8,24 @@ class Api::V1::LocationsController < ApplicationController
     render json: SearchResultSerializer.new(locations)
   end
 
+  def show
+    location = DetailedLocationFacade.new(params[:id]).location
+    if location
+      render json: DetailedLocationSerializer.new(location)
+    else 
+      no_location_response
+    end
+  end
+
   private
   def search_params
-    params.permit(:name, :city)
+    params.permit(:name, :city, :id)
+  end
+
+  def no_location_response
+    render json: ErrorSerializer.new(
+    ErrorMessage.new(
+      "The requested business could not be found.", 404
+    )).serialize_json, status: 404
   end
 end
