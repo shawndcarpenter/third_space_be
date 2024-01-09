e118f99fb77ff5bbc804dcec17af03599c382b4d
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-09 14:18:19 -0700

    Remove all mentions of markers and re-add tags column

--- app/serializers/third_space_serializer.rb
+++ app/serializers/third_space_serializer.rb
@@ -1,5 +1,4 @@
 class ThirdSpaceSerializer
   include JSONAPI::Serializer
-  attributes :yelp_id, :name, :address, :rating, :phone, :photos, :lat, :lon, :price, :hours, :category, :open_now, :markers
-  has_many :markers, include_data: true
+  attributes :yelp_id, :name, :address, :rating, :phone, :photos, :lat, :lon, :price, :hours, :category, :open_now, :tags
 end
