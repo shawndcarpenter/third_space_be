39e53bae0e3dd4b33930e6237370a8d4553f1bec
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-09 18:12:10 -0700

    Add methods for third space poros to make objects when not all data is available

--- app/poros/third_space_poro.rb
+++ app/poros/third_space_poro.rb
@@ -25,9 +25,43 @@ class ThirdSpacePoro
     @lat = data[:coordinates][:latitude]
     @lon = data[:coordinates][:longitude]
     @price = data[:price]
-    @hours = data[:hours]
-    @open_now = data[:hours].first[:is_open_now]  #Need to Clean this Up
+    @hours = provide_hours(data)
+    @open_now = provide_open_now(data)
     @category = data[:categories].first[:title]
     @tags = tags
+  end
+
+  def provide_hours(data)
+    if data[:hours]
+      data[:hours]
+    else 
+      "Hours not provided."
+    end
+  end
+
+  def provide_open_now(data)
+    if data[:hours].first[:is_open_now]
+      data[:hours].first[:is_open_now]
+    else 
+      "Open status not provided."
+    end
+  end
+
+  def make_third_space
+    ThirdSpace.create!({
+    yelp_id: @yelp_id, 
+    name: @name, 
+    address: @address, 
+    rating: @rating, 
+    phone: @phone, 
+    photos: @photos, 
+    lat: @lat, 
+    lon: @lon, 
+    price: @price, 
+    hours: @hours,
+    open_now: @open_now,
+    category: @category, 
+    tags: @tags
+       })
   end
 end
