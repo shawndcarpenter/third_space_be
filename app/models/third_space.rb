697c5181875b69a74ca441072becc9fff5df9bc9
Shawn Carpenter <shawncarpenter.co@gmail.com>
2024-01-24 18:21:00 -0700

    Add dependent destroy for third space markers

--- app/models/third_space.rb
+++ app/models/third_space.rb
@@ -2,5 +2,5 @@ class ThirdSpace < ApplicationRecord
   has_many :user_third_spaces
   has_many :users, through: :user_third_spaces
   has_many :review_objects
-  has_many :markers
+  has_many :markers, dependent: :destroy
 end
