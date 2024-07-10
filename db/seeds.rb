0c06eebc9765074b760707b15f2a82370546f599
shawndcarpenter <shawncarpenter.co@gmail.com>
2024-07-09 18:30:25 -0600

    Create rake task for data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,81 +1,82 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-require "factory_bot_rails"
-include FactoryBot::Syntax::Methods
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-boulder_locations = File.read('spec/fixtures/boulder_locations_top_50_results_search.json')
-boulder_json = JSON.parse(boulder_locations, symbolize_names: true)
-boulder_businesses = boulder_json[:businesses]
+# bundle exec rake dev:seed
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# require "factory_bot_rails"
+# include FactoryBot::Syntax::Methods
+# #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+# #   Character.create(name: "Luke", movie: movies.first)
+# boulder_locations = File.read('spec/fixtures/boulder_locations_top_50_results_search.json')
+# boulder_json = JSON.parse(boulder_locations, symbolize_names: true)
+# boulder_businesses = boulder_json[:businesses]
 
-boulder_businesses.map do |business|
-  poro = ThirdSpacePoro.new(business)
-  third_space = poro.make_third_space
-  reviews = FactoryBot.build_list(:review_object, 10, third_space: third_space, yelp_id: third_space.yelp_id)
-  reviews.each { |review| review.save }
-end
+# boulder_businesses.map do |business|
+#   poro = ThirdSpacePoro.new(business)
+#   third_space = poro.make_third_space
+#   reviews = FactoryBot.build_list(:review_object, 10, third_space: third_space, yelp_id: third_space.yelp_id)
+#   reviews.each { |review| review.save }
+# end
 
-austin_locations = File.read('spec/fixtures/austin_locations_top_50_results_search.json')
-austin_json = JSON.parse(austin_locations, symbolize_names: true)
-austin_businesses = austin_json[:businesses]
+# austin_locations = File.read('spec/fixtures/austin_locations_top_50_results_search.json')
+# austin_json = JSON.parse(austin_locations, symbolize_names: true)
+# austin_businesses = austin_json[:businesses]
 
-austin_businesses.map do |business|
-  poro = ThirdSpacePoro.new(business)
-  poro.make_third_space
-end
+# austin_businesses.map do |business|
+#   poro = ThirdSpacePoro.new(business)
+#   poro.make_third_space
+# end
 
-denver_locations = File.read('spec/fixtures/denver_locations_top_50_results_search.json')
-denver_json = JSON.parse(denver_locations, symbolize_names: true)
-denver_businesses = denver_json[:businesses]
+# denver_locations = File.read('spec/fixtures/denver_locations_top_50_results_search.json')
+# denver_json = JSON.parse(denver_locations, symbolize_names: true)
+# denver_businesses = denver_json[:businesses]
 
-denver_businesses.map do |business|
-  poro = ThirdSpacePoro.new(business)
-  poro.make_third_space
-end
+# denver_businesses.map do |business|
+#   poro = ThirdSpacePoro.new(business)
+#   poro.make_third_space
+# end
 
-minn_locations = File.read('spec/fixtures/minn_locations_top_50_results_search.json')
-minn_json = JSON.parse(minn_locations, symbolize_names: true)
-minn_businesses = minn_json[:businesses]
+# minn_locations = File.read('spec/fixtures/minn_locations_top_50_results_search.json')
+# minn_json = JSON.parse(minn_locations, symbolize_names: true)
+# minn_businesses = minn_json[:businesses]
 
-minn_businesses.map do |business|
-  poro = ThirdSpacePoro.new(business)
-  poro.make_third_space
-end
+# minn_businesses.map do |business|
+#   poro = ThirdSpacePoro.new(business)
+#   poro.make_third_space
+# end
 
-ThirdSpace.all.each_with_index do |space, index|
-  if index % 10 == 0
-    space.tags = ["Chill", "Loud", "Happy", "Studious", "Happy", "Studious", "Happy", "Studious", "Happy", "Studious", "Happy", "Studious", "Happy", "Studious", "Customer Restrooms", "Parking", "Parking", "Parking", "Sober", "No Purchase Necessary", "Child Friendly", "Child Friendly", "Bright", "Transportation Close", "BIPOC Friendly", "Queer Friendly", "Respectful", "Gender Neutral Restrooms"]
-  elsif index % 9 == 0
-    space.tags = ["Quiet", "Happy", "Studious", "Happy", "Studious", "Accessible Entrance", "Accessible Entrance", "Accessible Entrance", "Accessible Entrance", "Customer Restrooms", "Customer Restrooms", "Parking", "Sober", "Purchase Necessary", "Purchase Necessary", "Purchase Necessary", "Child Friendly", "Child Friendly",  "Bright", "No Transportation Close", "BIPOC Friendly", "Queer Friendly", "Respectful", "Gender Neutral Restrooms"]
-  elsif index % 8 == 0
-    space.tags = ["Chill", "Average Noise", "Studious", "Party", "Party", "Party", "Not Accessible", "Accessible Entrance", "Customer Restrooms", "Parking", "Sober", "No Purchase Necessary", "No Purchase Necessary", "No Purchase Necessary", "Bright","Transportation Close", "BIPOC Friendly", "Respectful", "Gender Neutral Restrooms"]
-  elsif index % 7 == 0
-    space.tags = ["Social", "Quiet", "Party", "Party", "Party", "Party", "Party", "Party", "Accessible Entrance", "Accessible Entrance", "Accessible Entrance", "No Customer Restrooms", "No Parking",  "No Parking", "Sober", "Purchase Necessary", "Purchase Necessary", "No Purchase Necessary", "Moody", "No Transportation Close", "Queer Friendly", "Responsive", "Gender Neutral Restrooms"]
-  elsif index % 6 == 0
-    space.tags = ["Average Noise", "Party", "Party", "Party", "Not Accessible", "Customer Restrooms",  "No Customer Restrooms", "Parking", "No Parking",  "No Parking", "Purchase Necessary", "Purchase Necessary", "Purchase Necessary", "Not Child Friendly", "Moody", "Transportation Close", "BIPOC Friendly", "Responsive", "No Gender Neutral Restrooms"]
-  elsif index % 5 == 0
-    space.tags = ["Chill", "Loud", "Party", "Sad", "Sad", "Sad", "Sad", "Accessible Entrance", "Accessible Entrance", "Accessible Entrance", "No Customer Restrooms", "No Parking",  "No Parking", "Sober", "Sober", "Sober","Purchase Necessary", "Purchase Necessary", "Purchase Necessary", "Child Friendly", "Child Friendly",  "Moody", "No Transportation Close", "BIPOC Friendly", "Pushy", "No Gender Neutral Restrooms"]
-  elsif index % 4 == 0
-    space.tags = ["Social", "Average Noise", "Party", "Party", "Party", "Not Accessible", "Accessible Entrance", "Accessible Entrance", "Customer Restrooms", "No Customer Restrooms",  "Non-Sober", "Purchase Necessary", "Purchase Necessary", "No Purchase Necessary", "Not Child Friendly", "Average Lighting", "Transportation Close", "Transportation Close", "BIPOC Friendly", "Queer Friendly", "Responsive", "No Gender Neutral Restrooms"]
-  elsif index % 3 == 0
-    space.tags = ["Average Noise", "Studious", "Studious", "Studious", "Sad", "Sad", "Sad", "Accessible Entrance",  "Accessible Entrance", "No Customer Restrooms", "Parking", "No Parking", "Sober", "Non-Sober", "No Purchase Necessary", "Child Friendly", "Child Friendly", "Average Lighting", "No Transportation Close", "Not BIPOC Friendly", "Pushy", "Responsive", "Responsive", "No Gender Neutral Restrooms"]
-  elsif index % 2 == 0
-    space.tags = ["Social", "Quiet", "Studious", "Studious", "Sad", "Sad", "Not Accessible", "Accessible Entrance", "Not Accessible", "Customer Restrooms", "No Customer Restrooms", "No Customer Restrooms", "No Parking", "Sober", "Purchase Necessary", "Not Child Friendly", "Average Lighting", "Transportation Close", "No Transportation Close", "BIPOC Friendly", "BIPOC Friendly", "Queer Friendly", "Pushy", "No Gender Neutral Restrooms"]
-  end
+# ThirdSpace.all.each_with_index do |space, index|
+#   if index % 10 == 0
+#     space.tags = ["Chill", "Loud", "Happy", "Studious", "Happy", "Studious", "Happy", "Studious", "Happy", "Studious", "Happy", "Studious", "Happy", "Studious", "Customer Restrooms", "Parking", "Parking", "Parking", "Sober", "No Purchase Necessary", "Child Friendly", "Child Friendly", "Bright", "Transportation Close", "BIPOC Friendly", "Queer Friendly", "Respectful", "Gender Neutral Restrooms"]
+#   elsif index % 9 == 0
+#     space.tags = ["Quiet", "Happy", "Studious", "Happy", "Studious", "Accessible Entrance", "Accessible Entrance", "Accessible Entrance", "Accessible Entrance", "Customer Restrooms", "Customer Restrooms", "Parking", "Sober", "Purchase Necessary", "Purchase Necessary", "Purchase Necessary", "Child Friendly", "Child Friendly",  "Bright", "No Transportation Close", "BIPOC Friendly", "Queer Friendly", "Respectful", "Gender Neutral Restrooms"]
+#   elsif index % 8 == 0
+#     space.tags = ["Chill", "Average Noise", "Studious", "Party", "Party", "Party", "Not Accessible", "Accessible Entrance", "Customer Restrooms", "Parking", "Sober", "No Purchase Necessary", "No Purchase Necessary", "No Purchase Necessary", "Bright","Transportation Close", "BIPOC Friendly", "Respectful", "Gender Neutral Restrooms"]
+#   elsif index % 7 == 0
+#     space.tags = ["Social", "Quiet", "Party", "Party", "Party", "Party", "Party", "Party", "Accessible Entrance", "Accessible Entrance", "Accessible Entrance", "No Customer Restrooms", "No Parking",  "No Parking", "Sober", "Purchase Necessary", "Purchase Necessary", "No Purchase Necessary", "Moody", "No Transportation Close", "Queer Friendly", "Responsive", "Gender Neutral Restrooms"]
+#   elsif index % 6 == 0
+#     space.tags = ["Average Noise", "Party", "Party", "Party", "Not Accessible", "Customer Restrooms",  "No Customer Restrooms", "Parking", "No Parking",  "No Parking", "Purchase Necessary", "Purchase Necessary", "Purchase Necessary", "Not Child Friendly", "Moody", "Transportation Close", "BIPOC Friendly", "Responsive", "No Gender Neutral Restrooms"]
+#   elsif index % 5 == 0
+#     space.tags = ["Chill", "Loud", "Party", "Sad", "Sad", "Sad", "Sad", "Accessible Entrance", "Accessible Entrance", "Accessible Entrance", "No Customer Restrooms", "No Parking",  "No Parking", "Sober", "Sober", "Sober","Purchase Necessary", "Purchase Necessary", "Purchase Necessary", "Child Friendly", "Child Friendly",  "Moody", "No Transportation Close", "BIPOC Friendly", "Pushy", "No Gender Neutral Restrooms"]
+#   elsif index % 4 == 0
+#     space.tags = ["Social", "Average Noise", "Party", "Party", "Party", "Not Accessible", "Accessible Entrance", "Accessible Entrance", "Customer Restrooms", "No Customer Restrooms",  "Non-Sober", "Purchase Necessary", "Purchase Necessary", "No Purchase Necessary", "Not Child Friendly", "Average Lighting", "Transportation Close", "Transportation Close", "BIPOC Friendly", "Queer Friendly", "Responsive", "No Gender Neutral Restrooms"]
+#   elsif index % 3 == 0
+#     space.tags = ["Average Noise", "Studious", "Studious", "Studious", "Sad", "Sad", "Sad", "Accessible Entrance",  "Accessible Entrance", "No Customer Restrooms", "Parking", "No Parking", "Sober", "Non-Sober", "No Purchase Necessary", "Child Friendly", "Child Friendly", "Average Lighting", "No Transportation Close", "Not BIPOC Friendly", "Pushy", "Responsive", "Responsive", "No Gender Neutral Restrooms"]
+#   elsif index % 2 == 0
+#     space.tags = ["Social", "Quiet", "Studious", "Studious", "Sad", "Sad", "Not Accessible", "Accessible Entrance", "Not Accessible", "Customer Restrooms", "No Customer Restrooms", "No Customer Restrooms", "No Parking", "Sober", "Purchase Necessary", "Not Child Friendly", "Average Lighting", "Transportation Close", "No Transportation Close", "BIPOC Friendly", "BIPOC Friendly", "Queer Friendly", "Pushy", "No Gender Neutral Restrooms"]
+#   end
 
-  reviews = FactoryBot.build_list(:review_object, 5, third_space: space, yelp_id: space.yelp_id)
-  reviews.each { |review| review.save }
-  space.save
-end
+#   reviews = FactoryBot.build_list(:review_object, 5, third_space: space, yelp_id: space.yelp_id)
+#   reviews.each { |review| review.save }
+#   space.save
+# end
 
-ThirdSpace.all.each do |space|
-  if space.tags != nil
-    space.tags.map do |tag|
-      space.markers.create!(name: tag)
-    end
-  end
-end
+# ThirdSpace.all.each do |space|
+#   if space.tags != nil
+#     space.tags.map do |tag|
+#       space.markers.create!(name: tag)
+#     end
+#   end
+# end
 
-user = User.create
+# user = User.create
---
+++ lib/tasks/dev.rake
@@ -0,0 +1,88 @@
+namespace :dev do
+  desc "Fill the database with sample data for development"
+  task seed: :environment do
+    require "factory_bot_rails"
+    Marker.destroy_all
+    ReviewObject.destroy_all
+    UserThirdSpace.destroy_all
+    ThirdSpace.destroy_all
+    User.destroy_all
+    include FactoryBot::Syntax::Methods
+
+    boulder_locations = File.read('spec/fixtures/boulder_locations_top_50_results_search.json')
+    boulder_json = JSON.parse(boulder_locations, symbolize_names: true)
+    boulder_businesses = boulder_json[:businesses]
+    
+    boulder_businesses.map do |business|
+      poro = ThirdSpacePoro.new(business)
+      third_space = poro.make_third_space
+      reviews = FactoryBot.build_list(:review_object, 10, third_space: third_space, yelp_id: third_space.yelp_id)
+      reviews.each { |review| review.save }
+    end
+    
+    austin_locations = File.read('spec/fixtures/austin_locations_top_50_results_search.json')
+    austin_json = JSON.parse(austin_locations, symbolize_names: true)
+    austin_businesses = austin_json[:businesses]
+    
+    austin_businesses.map do |business|
+      poro = ThirdSpacePoro.new(business)
+      poro.make_third_space
+    end
+    
+    denver_locations = File.read('spec/fixtures/denver_locations_top_50_results_search.json')
+    denver_json = JSON.parse(denver_locations, symbolize_names: true)
+    denver_businesses = denver_json[:businesses]
+    
+    denver_businesses.map do |business|
+      poro = ThirdSpacePoro.new(business)
+      poro.make_third_space
+    end
+    
+    minn_locations = File.read('spec/fixtures/minn_locations_top_50_results_search.json')
+    minn_json = JSON.parse(minn_locations, symbolize_names: true)
+    minn_businesses = minn_json[:businesses]
+    
+    minn_businesses.map do |business|
+      poro = ThirdSpacePoro.new(business)
+      poro.make_third_space
+    end
+    
+    ThirdSpace.all.each_with_index do |space, index|
+      if index % 10 == 0
+        space.tags = ["Chill", "Loud", "Happy", "Studious", "Happy", "Studious", "Happy", "Studious", "Happy", "Studious", "Happy", "Studious", "Happy", "Studious", "Customer Restrooms", "Parking", "Parking", "Parking", "Sober", "No Purchase Necessary", "Child Friendly", "Child Friendly", "Bright", "Transportation Close", "BIPOC Friendly", "Queer Friendly", "Respectful", "Gender Neutral Restrooms"]
+      elsif index % 9 == 0
+        space.tags = ["Quiet", "Happy", "Studious", "Happy", "Studious", "Accessible Entrance", "Accessible Entrance", "Accessible Entrance", "Accessible Entrance", "Customer Restrooms", "Customer Restrooms", "Parking", "Sober", "Purchase Necessary", "Purchase Necessary", "Purchase Necessary", "Child Friendly", "Child Friendly",  "Bright", "No Transportation Close", "BIPOC Friendly", "Queer Friendly", "Respectful", "Gender Neutral Restrooms"]
+      elsif index % 8 == 0
+        space.tags = ["Chill", "Average Noise", "Studious", "Party", "Party", "Party", "Not Accessible", "Accessible Entrance", "Customer Restrooms", "Parking", "Sober", "No Purchase Necessary", "No Purchase Necessary", "No Purchase Necessary", "Bright","Transportation Close", "BIPOC Friendly", "Respectful", "Gender Neutral Restrooms"]
+      elsif index % 7 == 0
+        space.tags = ["Social", "Quiet", "Party", "Party", "Party", "Party", "Party", "Party", "Accessible Entrance", "Accessible Entrance", "Accessible Entrance", "No Customer Restrooms", "No Parking",  "No Parking", "Sober", "Purchase Necessary", "Purchase Necessary", "No Purchase Necessary", "Moody", "No Transportation Close", "Queer Friendly", "Responsive", "Gender Neutral Restrooms"]
+      elsif index % 6 == 0
+        space.tags = ["Average Noise", "Party", "Party", "Party", "Not Accessible", "Customer Restrooms",  "No Customer Restrooms", "Parking", "No Parking",  "No Parking", "Purchase Necessary", "Purchase Necessary", "Purchase Necessary", "Not Child Friendly", "Moody", "Transportation Close", "BIPOC Friendly", "Responsive", "No Gender Neutral Restrooms"]
+      elsif index % 5 == 0
+        space.tags = ["Chill", "Loud", "Party", "Sad", "Sad", "Sad", "Sad", "Accessible Entrance", "Accessible Entrance", "Accessible Entrance", "No Customer Restrooms", "No Parking",  "No Parking", "Sober", "Sober", "Sober","Purchase Necessary", "Purchase Necessary", "Purchase Necessary", "Child Friendly", "Child Friendly",  "Moody", "No Transportation Close", "BIPOC Friendly", "Pushy", "No Gender Neutral Restrooms"]
+      elsif index % 4 == 0
+        space.tags = ["Social", "Average Noise", "Party", "Party", "Party", "Not Accessible", "Accessible Entrance", "Accessible Entrance", "Customer Restrooms", "No Customer Restrooms",  "Non-Sober", "Purchase Necessary", "Purchase Necessary", "No Purchase Necessary", "Not Child Friendly", "Average Lighting", "Transportation Close", "Transportation Close", "BIPOC Friendly", "Queer Friendly", "Responsive", "No Gender Neutral Restrooms"]
+      elsif index % 3 == 0
+        space.tags = ["Average Noise", "Studious", "Studious", "Studious", "Sad", "Sad", "Sad", "Accessible Entrance",  "Accessible Entrance", "No Customer Restrooms", "Parking", "No Parking", "Sober", "Non-Sober", "No Purchase Necessary", "Child Friendly", "Child Friendly", "Average Lighting", "No Transportation Close", "Not BIPOC Friendly", "Pushy", "Responsive", "Responsive", "No Gender Neutral Restrooms"]
+      elsif index % 2 == 0
+        space.tags = ["Social", "Quiet", "Studious", "Studious", "Sad", "Sad", "Not Accessible", "Accessible Entrance", "Not Accessible", "Customer Restrooms", "No Customer Restrooms", "No Customer Restrooms", "No Parking", "Sober", "Purchase Necessary", "Not Child Friendly", "Average Lighting", "Transportation Close", "No Transportation Close", "BIPOC Friendly", "BIPOC Friendly", "Queer Friendly", "Pushy", "No Gender Neutral Restrooms"]
+      end
+    
+      reviews = FactoryBot.build_list(:review_object, 5, third_space: space, yelp_id: space.yelp_id)
+      reviews.each { |review| review.save }
+      space.save
+    end
+    
+    ThirdSpace.all.each do |space|
+      if space.tags != nil
+        space.tags.map do |tag|
+          space.markers.create!(name: tag)
+        end
+      end
+    end
+    
+    user = User.create
+  end
+
+  "third spaces loaded"
+end
